47a799e69a03b6b3deb535ddce7c2f6d7076441d
Matt Boynes <mboynes@alleyinteractive.com>
2014-06-12 13:43:42 -0400

    Get ES working in Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,6 +13,23 @@ function _manually_load_plugin() {
 	SP_Config()->update_settings( array( 'active' => true, 'must_init' => false, 'host' => $host ) );
 	SP_API()->index = 'searchpress-tests';
 
+	// Make sure ES is running and responding
+	$tries = 5;
+	$sleep = 3;
+	do {
+		$response = wp_remote_get( 'http://localhost:9200/' );
+		if ( '200' == wp_remote_retrieve_response_code( $response ) ) {
+			// Looks good!
+			break;
+		} else {
+			printf( "\nInvalid response from ES (%s), sleeping %d seconds and trying again...\n", wp_remote_retrieve_response_code( $response ), $sleep );
+			sleep( $sleep );
+		}
+	} while ( --$tries );
+
+	// If we didn't end with a 200 status code, exit
+	sp_tests_verify_response_code( $response );
+
 	// These were not added because SP wasn't active when SP_Sync_Manager was loaded
 	add_action( 'save_post',    array( SP_Sync_Manager(), 'sync_post' ) );
 	add_action( 'delete_post',  array( SP_Sync_Manager(), 'delete_post' ) );
@@ -27,5 +44,15 @@ function sp_index_flush_data() {
 	SP_Config()->create_mapping();
 }
 
+
+function sp_tests_verify_response_code( $response ) {
+	if ( '200' != wp_remote_retrieve_response_code( $response ) ) {
+		printf( "Could not index posts!\nResponse code %s\n", wp_remote_retrieve_response_code( $response ) );
+		if ( is_wp_error( $response ) ) {
+			printf( "Message: %s\n", $response->get_error_message() );
+		}
+		exit( 1 );
+	}
+}
 
 require $_tests_dir . '/includes/bootstrap.php';
