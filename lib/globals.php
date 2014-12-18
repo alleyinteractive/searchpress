7d30f26fe0c8b254e02a9097a91afbf23b5e4977
Matt Boynes <mboynes@alleyinteractive.com>
2014-12-18 13:27:39 -0500

    Fix error reporting for inaccessible clusters

--- lib/globals.php
+++ lib/globals.php
@@ -10,5 +10,8 @@
  * Error Codes
  */
 
-# SP_Config()->flush() didn't respond with 200
+// SP_Config()->flush() didn't respond with 200
 define( 'SP_ERROR_FLUSH_FAIL', '100' );
+
+// SP_Heartbeat()->check_beat() failed
+define( 'SP_ERROR_NO_BEAT',    '101' );
