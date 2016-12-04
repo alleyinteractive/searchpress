f1620f36296a16dd67769f250d135db4b9aadcd5
Matt Boynes <mboynes@alleyinteractive.com>
2016-12-04 12:16:03 -0500

    Scriptify es install

---
+++ bin/install-es.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+if [ $# -lt 1 ]; then
+  echo "usage: $0 <es-version>"
+  exit 1
+fi
+
+ES_VERSION=$1
+
+setup_es() {
+  download_url=$1
+  mkdir /tmp/elasticsearch
+  wget -O - $download_url | tar xz --directory=/tmp/elasticsearch --strip-components=1
+}
+
+start_es() {
+  /tmp/elasticsearch/bin/elasticsearch $1 > /tmp/elasticsearch.log &
+  sleep 10
+  curl http://localhost:9200 && echo "ES is up!" || cat /tmp/elasticsearch.log
+}
+
+if [[ "$ES_VERSION" == 5.* ]]; then
+  setup_es https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION.tar.gz
+else
+  setup_es https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/$ES_VERSION/elasticsearch-$ES_VERSION.tar.gz
+fi
+
+# java_home='/usr/lib/jvm/java-8-oracle'
+if [[ "$ES_VERSION" == 5.* ]]; then
+  start_es '-Edefault.path.repo=/tmp'
+else
+  start_es '-Des.path.repo=/tmp'
+fi
