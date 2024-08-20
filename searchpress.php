4b08a0eeb0ff164cbf137de858254c4d7e458833
Greg Marshall <greg@alley.co>
2024-08-20 11:33:15 -0500

    change version, update changelog

--- searchpress.php
+++ searchpress.php
@@ -3,7 +3,7 @@
  * Plugin Name: SearchPress
  * Plugin URI: http://searchpress.org
  * Description: Elasticsearch integration for WordPress.
- * Version: 0.5
+ * Version: 0.5.1
  * Author: Matthew Boynes, Alley Interactive
  * Author URI: https://alley.com
  *
